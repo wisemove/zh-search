2614eb1683c39fe6adf38fb7cd5925716cce7d31
yunjume <yunjume@live.com>
2015-09-04 10:37:58 +0800

    用户登录模块的添加

---
+++ src/main/java/com/zhgw/search/model/index/UserService.java
@@ -0,0 +1,20 @@
+package com.zhgw.search.model.index;
+
+import org.springframework.stereotype.Service;
+
+import com.zhgw.search.common.Conditions;
+import com.zhgw.search.dao.CommonDao;
+
+@Service
+public class UserService extends CommonDao<UserEntity>{
+
+	
+	public UserEntity chkUser(String userName, String password) {
+
+		Conditions con = new Conditions();
+		con.eq("username", userName).eq("password", password);
+		UserEntity un = this.queryUnique(con);
+		
+		return un ;
+	}
+}
