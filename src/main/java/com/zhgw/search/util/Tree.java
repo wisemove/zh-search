0053e0fc8f7f57850d976f4366d6a75dbdffd090
yunjume <yunjume@live.com>
2015-09-29 16:56:33 +0800

    增加easyui tree

---
+++ src/main/java/com/zhgw/search/util/Tree.java
@@ -0,0 +1,13 @@
+package com.zhgw.search.util;
+
+public interface Tree {
+
+	long getId();
+
+	String getText();
+
+	String getState();
+
+	boolean isChecked ();
+
+}
---
+++ src/main/java/com/zhgw/search/util/TreeUtil.java
@@ -0,0 +1,47 @@
+package com.zhgw.search.util;
+
+import java.util.List;
+
+public class TreeUtil {
+
+	public static String toJson(List<? extends Tree> trees) {
+		StringBuffer sb = new StringBuffer();
+
+		
+		if(trees==null || trees.size()==0)
+			return "[{}]";
+		for (Tree tree : trees) {
+
+			sb.append("{");
+			sb.append("\"id\":");
+			sb.append(tree.getId());
+			sb.append(",");
+
+			sb.append("\"");
+			sb.append("text");
+			sb.append("\"");
+			sb.append(":");
+			sb.append("\"");
+			sb.append(tree.getText());
+			sb.append("\",");
+
+			sb.append("\"");
+			sb.append("state");
+			sb.append("\"");
+			sb.append(":");
+			sb.append("\"");
+			sb.append("closed");
+			sb.append("\"");
+
+			sb.append("}");
+
+			sb.append(",");
+		}
+
+		if (!sb.toString().isEmpty())
+			sb = sb.deleteCharAt(sb.length()-1);
+		
+		
+		return "["+sb.toString()+"]";
+	}
+}
